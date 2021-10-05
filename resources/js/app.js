8b894e820215d47cbe5b38ec7e3f3164a809aeb2
Oksana Pavlik <oksana.pavlik@admiralmarkets.com>
2021-10-05 19:39:29 +0300

    Set up basic frontend dev environment
    - Add npm dependencies
    - Create Vue component
    - Update webpack.mix.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import vue from 'vue'
+import TrustFlowScore from './components/TrustFlowScore.vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+window.Vue = vue;
+
+Vue.component('trust-flow-score', TrustFlowScore);
+
+new Vue({
+    el: '#app'
+});
---
+++ resources/js/components/TrustFlowScore.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-table striped hover :items="items"></b-table>
+    </div>
+</template>
+
+<script>
+import { BButton, BTable } from 'bootstrap-vue'
+export default {
+    components: {
+        'b-table': BTable
+    },
+    data() {
+        return {
+            items: []
+        }
+    },
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
